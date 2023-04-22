10be929ac2fbc0d4f6baa5e993146df5241b5c15
hozaifa <7ozaifakhalil@gmail.com>
2023-04-22 23:07:39 +0200

    bug fixed by changes attributes name in Dto and update README file

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # PostTask
 Steps for run the project 
 1- run the sql files to create mysql schima 
-2- add your dataSource(username and password and url) to the applicationProperties file 
+2- add your dataSource(username and password and url) to the resources/application.Properties file 
 3- run the project by command (mvn Spring-boot:run)
+4- you should create user in database like 
+ "userId": 1,
+ "userEmail": "ali@gmail.com",
+ "userName": "ali"
+
+5- you can test post method by this body :
+{
+        "user": {
+            "userId": 1,
+            "userEmail": "ali@gmail.com",
+            "userName": "ali"
+        },
+        "body": "new post body",
+        "tittle": "sport",
+        "id": 3
+}
 
 
 note:look to the ERD diagram.png  
--- src/main/java/com/NewTask/dto/PostDto.java
+++ src/main/java/com/NewTask/dto/PostDto.java
@@ -1,33 +1,36 @@
 package com.NewTask.dto;
 
+import com.NewTask.entity.dao.User;
+
 public class PostDto {
     private long Id;
-    private long userId;
+    private User user;
     private String body;
-    private String title;
+    private String tittle;
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
     public long getId() {
         return Id;
     }
     public void setId(long id) {
         Id = id;
     }
-    public long getUserId() {
-        return userId;
-    }
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
+ 
     public String getBody() {
         return body;
     }
     public void setBody(String body) {
         this.body = body;
     }
-    public String getTitle() {
-        return title;
+    public String getTittle() {
+        return tittle;
     }
-    public void setTitle(String title) {
-        this.title = title;
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
     }
 
     
